593aa1f582cf1bb2140d54a560df0572e193fdea
bobo8765432100-oss <bobo8765432100@gmail.com>
2026-07-24 16:48:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-cat app.py
+from flask import Flask, render_template, request
+import yt_dlp
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form.get('url')
+        # هنا بيكمل باقي الكود الخاص بتحميل الفيديوهات بتاعك
+        
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
